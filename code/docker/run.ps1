ac0d37f4f2e95d846057b318a90d3f72993c2972
Andy Kukuc <jeep98.ak@gmail.com>
2021-02-20 15:44:40 -0600

    might be able to fix this

--- code/docker/run.ps1
+++ code/docker/run.ps1
@@ -1,4 +1,4 @@
-docker run --rm -it --name fedora_test1 -v share:/share fedora_test1
+docker run --rm -it --name fedora_test1 -v C:\Users\Administrator\Documents\Coding\itm430_project\code\docker/share/:/share fedora_test1
 #docker run -it --name fedora_test1 fedora_test1
 #--name can only be used once there can only be 1
 #delete the name and run it as much as you want COMPUTER MIGHT DIE BETA TESTING 
