208c8a6c3c953bbb226f20b8a9843f5a06ef0119
Andy Kukuc <jeep98.ak@gmail.com>
2021-02-20 15:34:15 -0600

    this will work with bash or linux

--- code/docker/run.sh
+++ code/docker/run.sh
@@ -1,5 +1,5 @@
-#docker run --rm -it --name fedora_test1 -v share:/share fedora_test1
-docker run -it --name fedora_test1 fedora_test1
+docker run --rm -it --name fedora_test1 -v share:/share fedora_test1
+#docker run -it --name fedora_test1 fedora_test1
 #--name can only be used once there can only be 1
 #delete the name and run it as much as you want COMPUTER MIGHT DIE BETA TESTING 
 #beta you can control the container by import the Dockerfile somehow which is pretty dope
